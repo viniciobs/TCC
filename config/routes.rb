0144bc7cfa595a0ff989dcd8c84bdd94208c3b62
Vinício Barreto dos Santos <vinicio.barreto.santos@gmail.com>
2020-11-06 13:10:12 -0300

    definindo página inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+  	authenticated :user do
+		root 'products#index', as: :authenticated_root
+	end
+
+  	unauthenticated do
+  		root 'devise/sessions#new', as: :unauthenticated_root
+  	end
+  end
+
   resources :stocks
   resources :products
   resources :product_categories
