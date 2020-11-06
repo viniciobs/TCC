aa59cff65e6a82dcd68ecbc6b2620dc9d1603cf5
Vinício Barreto dos Santos <vinicio.barreto.santos@gmail.com>
2020-11-06 01:04:25 -0300

    do not know what the fuck I did but I am commiting it

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum user_type: { manager: 0, musician: 1, customer: 2 }
 end
