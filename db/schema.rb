7f628c187365531d55ebe9f75fcf9153ad3a6162
Vinício Barreto dos Santos <vinicio.barreto.santos@gmail.com>
2020-11-06 23:22:28 -0300

    adicionando opção de logout no menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_165143) do
+ActiveRecord::Schema.define(version: 2020_11_07_015829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_11_06_165143) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username", null: false
     t.integer "user_type", null: false
+    t.string "name", null: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
