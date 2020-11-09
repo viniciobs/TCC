11578a6dcae93ff286b6b1a9422ccbe0cba51c31
Vinício Barreto dos Santos <vinicio.barreto.santos@gmail.com>
2020-11-09 02:59:29 -0300

    removendo o layout do render 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,11 @@ class ApplicationController < ActionController::Base
 	end
 
 	def render_404
-		render file: "#{Rails.root}/public/404", status: :not_found
+		respond_to do |format|
+			format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
+			format.xml  { head :not_found }
+			format.any  { head :not_found }
+		end
 	end
 
 	private 
