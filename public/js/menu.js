2aab3ddf02a7b61ff40e970d5e8bb68ab5d88c7f
Vinício Barreto dos Santos <vinicio.barreto.santos@gmail.com>
2020-11-16 21:59:00 -0300

    Finalizado dinamica e estilo da tela de cardapio

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,5 +1,6 @@
 var swiper = new Swiper('.swiper-container', 
 {    
+    slideToClickedSlide: true,   
     centeredSlides: true,
     centeredSlidesBounds: true,
     slidesPerView: 3,
@@ -9,10 +10,10 @@ var swiper = new Swiper('.swiper-container',
     pagination: 
     { 
         el: ".swiper-pagination",
-        clickable: true 
+        clckable: true
     }
 });
 
-$('.food-menu .item a').click(function(e){ e.preventDefault(); });
+$('.food-menu .item a').click(function(e) { e.preventDefault(); });
 
 
