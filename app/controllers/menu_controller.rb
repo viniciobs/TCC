a0277bf83183c914c70da79f5de48f69b5b856af
Vinício Barreto dos Santos <vinicio.barreto.santos@gmail.com>
2020-11-16 22:07:25 -0300

    removido busca em cardápio

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -2,12 +2,12 @@ class MenuController < ApplicationController
 	before_action :check_user_permission, except: [:inactive]
 	
 	def index		
-		@categories = ProductCategory.all		
-  	end
+    @categories = ProductCategory.all
+  end
   	
-  	private 
-  	
-  	def check_user_permission
-    	render_404 if !current_user.nil? && current_user.user_type == 'manager'
-    end
+	private 
+	
+	def check_user_permission
+  	render_404 if !current_user.nil? && current_user.user_type == 'manager'
+  end
 end
