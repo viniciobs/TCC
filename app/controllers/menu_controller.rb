5038b0200677a2fd825fab643f60574138d28393
Vinício Barreto dos Santos <vinicio.barreto.santos@gmail.com>
2020-11-17 19:39:16 -0300

    Criando link do carrinho

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -2,12 +2,13 @@ class MenuController < ApplicationController
 	before_action :check_user_permission, except: [:inactive]
 	
 	def index		
-    @categories = ProductCategory.all
-  end
+		@categories = ProductCategory.all
+		@order = current_user.order
+	end
   	
 	private 
 	
 	def check_user_permission
-  	render_404 if !current_user.nil? && current_user.user_type == 'manager'
-  end
+  		render_404 if !current_user.nil? && current_user.user_type == 'manager'
+  	end
 end
